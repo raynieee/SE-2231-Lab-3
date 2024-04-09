4dc4a1d414f375331e4779fb4ff418e0eadff6c8
Raine Christine Perez <125470134+raynieee@users.noreply.github.com>
2024-04-09 19:22:54 +0800

    - updated searchNode class

--- src/classes/searchNode.ts
+++ src/classes/searchNode.ts
@@ -1,31 +1,27 @@
 import Board from "./board";
-import { MinHeap } from "min-heap-typed";
 
 class SearchNode {
-  //board: Board
+  board: Board
   moves: number
   previousSearchNode: SearchNode | null;
 
-  constructor(board: Board, moves: number, previousSearchNode: SearchNode | null = null) {
-    //this.board = board
+  constructor(
+    board: Board,
+    moves: number,
+    previousSearchNode: SearchNode | null = null
+  ) {
+    this.board = board
     this.moves = moves
     this.previousSearchNode = previousSearchNode
   }
 
-  priority(): number {
-    return this.moves; //+ this.board.hamming
+  manhattanPriority(): number {
+    return this.moves + this.board.manhattan();
+  }
+
+  hammingPriority(): number {
+    return this.moves + this.board.hamming();
   }
 }
 
-const heap = new MinHeap<SearchNode>([], {comparator: (a, b) => a.priority() - b.priority()});
-
-// const node1 = new SearchNode(13);
-// const node2 = new SearchNode(15);
-// const node3 = new SearchNode(8);
-
-// heap.add(node1);
-// heap.add(node2);
-// heap.add(node3);
-
-console.log(heap.poll());
-console.log(heap.peek());
+export default SearchNode;
