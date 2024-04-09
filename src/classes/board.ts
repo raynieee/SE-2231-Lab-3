1b603e4e43486b0f4a7d36967db48c96a141ca3c
Raine Christine Perez <125470134+raynieee@users.noreply.github.com>
2024-04-09 12:18:34 +0800

    - added isGoal and equals methods - updated hamming method (now handles blank tile) - changed toString method naming convention to displayBoard

--- src/classes/board.ts
+++ src/classes/board.ts
@@ -4,17 +4,17 @@ class Board {
 
   constructor(tiles: number[][]) {
     this.n = tiles.length;
-    this.tiles = tiles.map((row) => [...row]);  
+    this.tiles = tiles.map((row) => [...row]);
     // creates a copy of the tiles array, ensures modifications made to copied array don't affect original array
   }
 
-  toString(): string {
+  displayBoard(): string {
     let boardRep = "";  // string representation of this board
     for (let i = 0; i < this.n; i++) {  // iteration over the rows
       for (let j = 0; j < this.n; j++) {  // iteration over the columns
-        boardRep += this.tiles[i][j] + ' ';  // tiles numbered from 1 to n^2 - 1, places a space after each num
+        boardRep += this.tiles[i][j] + " ";  // tiles numbered from 1 to n^2 - 1, places a space after each num
       }
-      boardRep += '\n';  // next line after iterating through n columns
+      boardRep += "\n";  // next line after iterating through n columns
     }
     return boardRep;
   }
@@ -27,12 +27,22 @@ class Board {
     let count = 0;
     for (let i = 0; i < this.n; i++) {
       for (let j = 0; j < this.n; j++) {
-        if (this.tiles[i][j] !== i * this.n + j + 1) {
+        let goalPlacement = i * this.n + j + 1;
+        if (goalPlacement === this.n * this.n) {
+          goalPlacement = 0; // for the last tile
+        }
+        if (this.tiles[i][j] !== goalPlacement) {
           count++;
         }
       }
     }
-    return count;
+    let finalCount = 0;
+    if (this.tiles[this.n - 1][this.n - 1] === 0) {
+      finalCount = count - 1; // minus one if blank space is in the goal position
+    } else {
+      finalCount = count; // otherwise, this
+    }
+    return finalCount;
   }
 
   // sum of Manhattan distances between tiles and goal
@@ -48,20 +58,34 @@ class Board {
     return manhattanDistance;
   }
 
-  // is this board the goal board?
   isGoal(): boolean {
-    for (let i = 0; i < this.n; i++) { // rows
-      for (let j = 0; j < this.n; j++) { // columns
-        
+    // is this board the goal board?
+    for (let i = 0; i < this.n; i++) {
+      for (let j = 0; j < this.n; j++) {
+        if (this.tiles[i][j] !== 0 && this.tiles[i][j] !== i * this.n + j + 1) {
+          return false; // if tiles are not in correct position
+        }
       }
     }
-    return true;
+    return true; // if tiles are in correct position
   }
 
-  // does this board equal y?
   equals(y: Board): boolean {
-    // PLS MODIFY
-    return true;
+    // does this board equal y?
+    if (y.n !== this.n) {
+      return false; // not of same dimension, therefore automatic unequal
+    }
+
+    for (let i = 0; i < this.n; i++) {
+      // rows
+      for (let j = 0; j < this.n; j++) {
+        // columns
+        if (this.tiles[i][j] !== y.tiles[i][j]) {
+          return false; // if tiles in current board don't match with y
+        }
+      }
+    }
+    return true; // if current board is equal to y
   }
 
   // all neighboring boards
